d9423ca02db27fbe24cfb5c4b899ac4dd1dd3d64
Kajal <Kajal.mittal08@gmail.com>
2023-01-30 21:18:23 +0530

    Done changes

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-let productinfo = document.querySelector('.product-info')
+let productinfo = document.querySelector('.products-info');
 let productpage = productinfo.querySelectorAll('.info');
-document.querySelectorAll('.section .product-container .product .box').forEach(box => {
-    box.onclick = () => {
-        let name = box.getAttribute('data-name');
+document.querySelectorAll('.section .product-container .products .box .name').forEach(name => {
+    name.onclick = () => {
+        productinfo.style.display = 'flex';
+        let name = name.getAttribute('data-name');
         productpage.forEach(info => {
             let target = info.getAttribute('data-target');
             if (name == target) {
